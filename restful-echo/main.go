3845ec6343168dd71bdd6927608697cb8ba7d05e
Erdem Kara <erdem8kara@gmail.com>
2023-05-20 21:09:14 +0200

    improve examples

--- restful-echo/main.go
+++ restful-echo/main.go
@@ -1,67 +1,168 @@
 package main
 
 import (
+    "strconv"
 	"net/http"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-// album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+type link struct {
+    Index           int     `json:"index"`
+    LocalAddress    string  `json:"localAddress"`
+    RemoteAddress   string  `json:"remoteAddress"`
+    State           string  `json:"state"`
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+type stat struct {
+    Index           int     `json:"index"`
+    PacketCount     int     `json:"packetCount"`
+    DataSize        int     `json:"dataSize"`
+    AverageRTT      float64 `json:"averageRTT"`
 }
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c echo.Context) error {
-	var newAlbum album
+var links = []link{
+    {Index: 1, LocalAddress: "10.10.10.10:9000", RemoteAddress: "20.20.20.20:9000", State: "Active"},
+    {Index: 2, LocalAddress: "10.10.10.10:9001", RemoteAddress: "20.20.20.20:9001", State: "Inactive"},
+    {Index: 3, LocalAddress: "10.10.10.10:9002", RemoteAddress: "20.20.20.20:9002", State: "Active"},
+}
 
-	if err := c.Bind(&newAlbum); err != nil {
-		return c.JSON(http.StatusBadRequest, "Invalid request payload")
+var stats = []stat{
+    {Index: 1, PacketCount: 600, DataSize: 256, AverageRTT: 12.5},
+    {Index: 2, PacketCount: 700, DataSize: 512, AverageRTT: 5.2},
+    {Index: 3, PacketCount: 800, DataSize: 1024, AverageRTT: 1.3},
+}
+
+func getLinks(c echo.Context) error {
+    return c.JSON(http.StatusOK, links)
+}
+
+func getLinkByIndex(c echo.Context) error {
+    index := c.Param("index")
+    linkIndex, err := strconv.Atoi(index) // string to int
+
+    if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid link index")
 	}
 
-	albums = append(albums, newAlbum)
-    return c.JSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c echo.Context) error {
-    return c.JSON(http.StatusOK, albums)
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c echo.Context) error {
-    id := c.Param("id")
-
-    // Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, a := range albums {
-        if a.ID == id {
-            return c.JSON(http.StatusOK, a)
+    for _, l := range links {
+        if l.Index == linkIndex {
+            return c.JSON(http.StatusOK, l)
         }
     }
-    return c.JSON(http.StatusNotFound, "album not found")
+    return c.JSON(http.StatusNotFound, "Link index does not exist")
+}
+
+func getLinkByLocalAddress(c echo.Context) error {
+    localAddress := c.Param("localAddress")
+    // TODO check address regex
+
+    for _, l := range links {
+        if l.LocalAddress == localAddress {
+            return c.JSON(http.StatusOK, l)
+        }
+    }
+    return c.JSON(http.StatusNotFound, "Invalid link local address")
+}
+
+// curl -X POST -H "Content-Type: application/json" -d "{\"state\": \"Inactive\"}" http://localhost:8081/links/1
+func updateLinkState(c echo.Context) error {
+	index := c.Param("index")
+	linkIndex, err := strconv.Atoi(index) // string to int
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid link index")
+	}
+
+    for i, l := range links {
+        if l.Index == linkIndex {
+            var updateReq struct {
+                State string `json:"state"`
+            }
+            if err := c.Bind(&updateReq); err != nil {
+                return c.JSON(http.StatusBadRequest, "Invalid request payload")
+            }
+	        l.State = updateReq.State
+            links[i] = l;
+	        return c.JSON(http.StatusOK, l)
+        }
+    }
+    // !!!!! Better error handling forced by Echo
+    return c.JSON(http.StatusBadRequest, "Link index does not exist")
+}
+
+func getStats(c echo.Context) error {
+    return c.JSON(http.StatusOK, stats)
+}
+
+func getStatByIndex(c echo.Context) error {
+    index := c.Param("index")
+    statIndex, err := strconv.Atoi(index) // string to int
+
+    if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid stat index")
+	}
+
+    for _, s := range stats {
+        if s.Index == statIndex {
+            return c.JSON(http.StatusOK, s)
+        }
+    }
+    return c.JSON(http.StatusNotFound, "Stat index does not exist")
+}
+
+// curl -X DELETE http://localhost:8081/stats
+func resetStats(c echo.Context) error {
+    for i, s := range stats {
+        s.PacketCount = 0
+        s.DataSize = 0
+        s.AverageRTT = 0.0
+        stats[i] = s;
+    }
+    return c.JSON(http.StatusOK, stats)
+}
+
+// curl -X DELETE http://localhost:8081/stats/1
+func resetStatByIndex(c echo.Context) error {
+    index := c.Param("index")
+    statIndex, err := strconv.Atoi(index) // string to int
+
+    if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid stat index")
+	}
+
+    for i, s := range stats {
+        if s.Index == statIndex {
+            s.PacketCount = 0
+            s.DataSize = 0
+            s.AverageRTT = 0.0
+            stats[i] = s;
+            return c.JSON(http.StatusOK, s)
+        }
+
+    }
+    return c.JSON(http.StatusNotFound, "Stat index does not exist")
 }
 
 func main() {
 	e := echo.New()
   
 	// Middleware
-	e.Use(middleware.Logger())
+    e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+        Format: "method=${method}, uri=${uri}, status=${status}\n",
+    }))
+	//e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-  
-	e.GET("/albums", getAlbums)
-	e.GET("/albums/:id", getAlbumByID)
-	e.POST("/albums", postAlbums)
-	e.Logger.Fatal(e.Start("localhost:8080"))
+
+    e.GET("/links", getLinks)
+    e.GET("/links/index/:index", getLinkByIndex)
+    e.GET("/links/localAddress/:localAddress", getLinkByLocalAddress)
+    e.POST("/links/:index", updateLinkState)
+
+    e.GET("/stats", getStats)
+    e.GET("/stats/:index", getStatByIndex)
+    e.DELETE("/stats", resetStats)
+    e.DELETE("/stats/:index", resetStatByIndex)
+
+	e.Logger.Fatal(e.Start("localhost:8081"))
 }
--- restful-gin/main.go
+++ restful-gin/main.go
@@ -1,65 +1,172 @@
 package main
 
 import (
+    "strconv"
 	"net/http"
     "github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+type link struct {
+    Index           int     `json:"index"`
+    LocalAddress    string  `json:"localAddress"`
+    RemoteAddress   string  `json:"remoteAddress"`
+    State           string  `json:"state"`
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+type stat struct {
+    Index           int     `json:"index"`
+    PacketCount     int     `json:"packetCount"`
+    DataSize        int     `json:"dataSize"`
+    AverageRTT      float64 `json:"averageRTT"`
 }
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-    var newAlbum album
-
-    // Call BindJSON to bind the received JSON to
-    // newAlbum.
-    if err := c.BindJSON(&newAlbum); err != nil {
-        return
-    }
-
-    // Add the new album to the slice.
-    albums = append(albums, newAlbum)
-    c.IndentedJSON(http.StatusCreated, newAlbum)
+var links = []link{
+    {Index: 1, LocalAddress: "10.10.10.10:9000", RemoteAddress: "20.20.20.20:9000", State: "Active"},
+    {Index: 2, LocalAddress: "10.10.10.10:9001", RemoteAddress: "20.20.20.20:9001", State: "Inactive"},
+    {Index: 3, LocalAddress: "10.10.10.10:9002", RemoteAddress: "20.20.20.20:9002", State: "Active"},
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
+var stats = []stat{
+    {Index: 1, PacketCount: 600, DataSize: 256, AverageRTT: 12.5},
+    {Index: 2, PacketCount: 700, DataSize: 512, AverageRTT: 5.2},
+    {Index: 3, PacketCount: 800, DataSize: 1024, AverageRTT: 1.3},
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-    id := c.Param("id")
+func getLinks(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, links)
+}
 
-    // Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, a := range albums {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
+func getLinkByIndex(c *gin.Context) {
+    index := c.Param("index")
+    linkIndex, err := strconv.Atoi(index) // string to int
+
+    if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid link index"})
+		return
+	}
+
+    for _, l := range links {
+        if l.Index == linkIndex {
+            c.IndentedJSON(http.StatusOK, l)
             return
         }
     }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Link index does not exist"})
+}
+
+func getLinkByLocalAddress(c *gin.Context) {
+    localAddress := c.Param("localAddress")
+    // TODO check address regex
+
+    for _, l := range links {
+        if l.LocalAddress == localAddress {
+            c.IndentedJSON(http.StatusOK, l)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Invalid link local address"})
+}
+
+// curl -X POST -H "Content-Type: application/json" -d "{\"state\": \"Inactive\"}" http://localhost:8080/links/1
+func updateLinkState(c *gin.Context) {
+	index := c.Param("index")
+	linkIndex, err := strconv.Atoi(index) // string to int
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid link index"})
+		return
+	}
+
+    for i, l := range links {
+        if l.Index == linkIndex {
+            var updateReq struct {
+                State string `json:"state"`
+            }
+            if err := c.BindJSON(&updateReq); err != nil {
+                c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+                return
+            }
+	        l.State = updateReq.State
+            links[i] = l;
+	        c.JSON(http.StatusOK, l)
+            return
+        }
+    }
+}
+
+func getStats(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, stats)
+}
+
+func getStatByIndex(c *gin.Context) {
+    index := c.Param("index")
+    statIndex, err := strconv.Atoi(index) // string to int
+
+    if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid stat index"})
+		return
+	}
+
+    for _, s := range stats {
+        if s.Index == statIndex {
+            c.IndentedJSON(http.StatusOK, s)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Stat index does not exist"})
+}
+
+// curl -X DELETE http://localhost:8080/stats
+func resetStats(c *gin.Context) {
+    for i, s := range stats {
+        s.PacketCount = 0
+        s.DataSize = 0
+        s.AverageRTT = 0.0
+        stats[i] = s;
+    }
+    c.JSON(http.StatusOK, stats)
+}
+
+// curl -X DELETE http://localhost:8080/stats/1
+func resetStatByIndex(c *gin.Context) {
+    index := c.Param("index")
+    statIndex, err := strconv.Atoi(index) // string to int
+
+    if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid stat index"})
+		return
+	}
+
+    for i, s := range stats {
+        if s.Index == statIndex {
+            s.PacketCount = 0
+            s.DataSize = 0
+            s.AverageRTT = 0.0
+            stats[i] = s;
+            c.JSON(http.StatusOK, s)
+            return
+        }
+
+    }
+    c.JSON(http.StatusNotFound, gin.H{"message": "Stat index does not exist"})
 }
 
 func main() {
     router := gin.Default()
-    router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+
+    // Middleware
+    router.Use(gin.Logger())
+    router.Use(gin.Recovery())
+
+    router.GET("/links", getLinks)
+    router.GET("/links/index/:index", getLinkByIndex)
+    router.GET("/links/localAddress/:localAddress", getLinkByLocalAddress)
+    router.POST("/links/:index", updateLinkState)
+
+    router.GET("/stats", getStats)
+    router.GET("/stats/:index", getStatByIndex)
+    router.DELETE("/stats", resetStats)
+    router.DELETE("/stats/:index", resetStatByIndex)
+
     router.Run("localhost:8080")
 }
